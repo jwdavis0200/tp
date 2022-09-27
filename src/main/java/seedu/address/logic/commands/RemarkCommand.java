8489fc1dae6f929b8857064164989da7ff6038c8
jwdavis0200 <jwdavis0200@gmail.com>
2022-09-28 00:00:49 +0800

    Add Incomplete RemarkCommand functionality.

---
+++ src/main/java/seedu/address/logic/commands/RemarkCommand.java
@@ -0,0 +1,16 @@
+package seedu.address.logic.commands;
+
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.Model;
+
+public class RemarkCommand extends Command {
+
+    public static final String COMMAND_WORD = "remark";
+    public static final String MESSAGE_USAGE = COMMAND_WORD;
+    public static final String MESSAGE_NOT_YET_IMPLEMENTED_YET = "Remark command not implemented yet";
+    
+    @Override
+    public CommandResult execute(Model model) throws CommandException {
+        throw new CommandException("MESSAGE_NOT_IMPLEMENTED_YET");
+    }
+}
